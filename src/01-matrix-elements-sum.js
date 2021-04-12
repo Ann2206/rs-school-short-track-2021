549543237373724e5e174d612771f91f12431cab
Ann2206 <53495105+Ann2206@users.noreply.github.com>
2021-04-13 02:13:02 +0300

    fix

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -2,14 +2,14 @@ function getMatrixElementsSum(matrix) {
   let arr0 = [];
   let sum = [];
   let result = 0;
-  for (var i = 0; i < matrix.length; i++) {
+  for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (!arr0.includes(j)) {
         sum.push(matrix[i][j]);
       }
     }
     arr0 = [];
-    for (var j = 0; j < matrix[i].length; j++) {
+    for (let j = 0; j < matrix[i].length; j++) {
       if (matrix[i][j] == 0) {
         arr0.push(j);
       }
