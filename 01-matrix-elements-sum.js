af6741bf211ecdfee05e5ed9b0cba393c610b387
Ann2206 <53495105+Ann2206@users.noreply.github.com>
2021-04-13 02:14:58 +0300

    fix

---
+++ 01-matrix-elements-sum.js
@@ -0,0 +1,23 @@
+function getMatrixElementsSum(matrix) {
+  var arr0 = [];
+  let sum = [];
+  let result = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (!arr0.includes(j)) {
+        sum.push(matrix[i][j]);
+      }
+    }
+    arr0 = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] == 0) {
+        arr0.push(j);
+      }
+    }
+  }
+  for (let i = 0; i < sum.length; i++) {
+    result += sum[i];
+  }
+  return result;
+}
+module.exports = getMatrixElementsSum;
